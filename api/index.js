aaf05a1e8b0edd48d8c728fd696a2d660382cf21
Josiel Souza <josielsouza.dj@gmail.com>
2023-03-02 20:28:15 -0300

    fix: cors

--- api/index.js
+++ api/index.js
@@ -1,66 +1,59 @@
 import express from "express";
 import { Router } from "express";
-//const cors = require("cors");
+import cors from 'cors';
 const app = express();
 const route = Router();
 
-//app.use(cors());
-//app.use(express.json());
+app.use(cors({
+  origin: '*'
+}));
+app.use(express.json());
 
+/* route.get("/", (req, res) => {
+  res.send("sucesso!");
+}); */
 
 route.get("/", (req, res) => {
-  res.send("sucesso!");
-});
-
-route.get("/contato", (req, res) => {
-  //simulando consulta do banco de dados
   const data = [
     {
       id: 1,
-      nome: "teste",
-      email: "josiel.dj@gmail.com",
-      createdAt: "21-11-2022",
+      nome: "José Maria da Silva",
+      email: "jose@gmail.com",
+      createdAt: "02-03-2023",
     },
     {
       id: 2,
-      nome: "Maria Sol MAria",
+      nome: "Maria Solange",
       email: "mariasol@gmail.com",
-      createdAt: "18-11-2022",
+      createdAt: "01-02-2023",
     },
     {
       id: 3,
-      nome: "Solange Duarte",
-      email: "solange@gmail.com",
+      nome: "Caio Duarte",
+      email: "caio@gmail.com",
       createdAt: "05-11-2022",
     },
     {
       id: 4,
-      nome: "Solange Duarte",
-      email: "solange@gmail.com",
-      createdAt: "05-11-2022",
+      nome: "Bernardo de Jesus",
+      email: "bernardo@gmail.com",
+      createdAt: "06-08-2022",
     },
     {
       id: 5,
       nome: "Josiel Souza",
-      email: "solange@gmail.com",
+      email: "josielsouza.dj@gmail.com",
       createdAt: "05-11-2022",
     },
-    {
-      id: 6,
-      nome: "Josiel Souza",
-      email: "solange@gmail.com",
-      createdAt: "05-11-2022",
-    }
   ];
   res.json({
-    //mostrar resultado da consulta
     sucesso: true,
     data,
   });
 });
 app.use(route);
-const port = process.env.PORT || 4000
+const port = process.env.PORT || 3333;
 
 app.listen(port, () => {
-    console.log('Server running in ' + port)
-})
+  console.log("Server running in " + port);
+});
