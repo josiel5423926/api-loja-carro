a8031d3d9ab90d59668d1d383673c5cba4ce26d6
Josiel Souza <josielsouza.dj@gmail.com>
2023-04-18 14:02:39 -0300

    Feat: add time

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import swaggerFile from "../swagger.json";
 
 server.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
+
 const port = 3333;
 
 server.listen(port, () => console.log("listening on port ", port));
